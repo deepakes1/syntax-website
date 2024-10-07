5c1927d3d6ccf8de5a696a3506602887dee2af3b
Deepak ES <deepakes2003@gmail.com>
2024-10-07 19:53:05 +0530

    5commit

--- src/App.jsx
+++ src/App.jsx
@@ -13,11 +13,11 @@ function App() {
       <div className="p-5">
         {/* clg name */}
        
-        <div className="second-color font-bold text-center md:flex flex-col items-center justify-center">
-          <h1 className='text-lg md:text-xl'>GOVERNMENT COLLEGE OF ENGINEERING ERODE</h1>
-          <h1 className='md:text-lg mt-1'>DEPARTMENT OF COMPUTER SCIENCE AND ENGINEERING</h1>
-          <h1 className='md:text-lg font-semibold mt-1'>PRODULY PRESENTS </h1>
-          <h1 className='text-3xl md:text-5xl font-bold mt-5 font-serif'>SYNTAX</h1>
+        <div className="second-color font-extrabold text-center md:flex flex-col items-center justify-center">
+          <h1 className='text-xl'>GOVERNMENT COLLEGE OF ENGINEERING ERODE</h1>
+          <h1 className='text-lg mt-1'>DEPARTMENT OF COMPUTER SCIENCE AND ENGINEERING</h1>
+          <h1 className='text-lg font-extrabold mt-1'>PRODULY PRESENTS </h1>
+          <h1 className='text-5xl font-bold mt-5 font-serif'>SYNTAX</h1>
         </div>
         
 
