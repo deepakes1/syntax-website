2643c3dfe97a3a4307fbcf1a94510894c7cf3366
Deepak ES <deepakes2003@gmail.com>
2024-10-07 19:36:26 +0530

    3 commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 
 import './App.css'
-import collegelogo from "../src/assets/collegelogo.jpg"
 
 import Navbar from './Parts/Navbar'
 function App() {
   
   return (
     <>
-      <div className="p-5">
-        <Navbar/>
 
+      <Navbar />
+
+      
+      <div className="p-5">
         {/* clg name */}
         <div className="flex gap-3 mt-5  md:mt-10 items-center">
           {/* <img src={collegelogo} className='h-16 w-16 object-cover rounded-full' alt="image" /> */}
