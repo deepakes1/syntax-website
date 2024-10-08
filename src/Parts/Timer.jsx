6ed370ef1ca5c890810d61d89b8b74297eb9f9f0
Deepak ES <deepakes2003@gmail.com>
2024-10-08 15:40:31 +0530

    12

--- src/Parts/Timer.jsx
+++ src/Parts/Timer.jsx
@@ -32,17 +32,39 @@ function Timer() {
 
   return (
     <div>
-      <h1>Countdown to March 1, 2024:</h1>
+      {/* <h1>Countdown to March 1, 2024:</h1>
       <p>
         {countdown.days}d {countdown.hours}h {countdown.minutes}m {countdown.seconds}s
-      </p>
+      </p> */}
       {countdown.days < 0 && countdown.hours < 0 && countdown.minutes < 0 && countdown.seconds < 0 ? (
         <p>EXPIRED</p>
       ) : null}
 
-      {/* <div className="w-[200px] h-[30px] bg-black">
+      <div className="flex flex-col items-center text-[#40377D] font-bold text-lg">
+        <div className="flex">
         
-      </div> */}
+        <div className=" border-black text-center w-[100px] p-5 border-b-2 border-r-2">
+          <h1 className='text-4xl'>{countdown.days}</h1>
+          <p>Days</p>
+        </div>
+        <div className=" border-black text-center w-[100px] p-5 border-b-2 ">
+          <h1 className='text-4xl'>{countdown.hours}</h1>
+          <p>Hours</p>
+        </div>
+      </div>
+      <div className="flex">
+        
+        <div className="border-black w-[100px] text-center p-5  border-r-2 ">
+          <h1 className='text-4xl'>{countdown.minutes}</h1>
+          <p>Minutes</p>
+        </div>
+        <div className="p-5 text-center w-[100px]">
+          <h1 className='text-4xl'>{countdown.seconds}</h1>
+          <p >Seconds</p>
+        </div>
+      </div>
+      </div>
+
     </div>
 
 
