38a0c1f32cb171a2d48436d54253ef191923207b
Deepak ES <deepakes2003@gmail.com>
2024-10-09 12:02:53 +0530

    17

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Navbar from './Parts/Navbar'
 import Timer from './Parts/Timer'
+import syntax from './assets/syntax.png'
 
 
 function App() {
@@ -13,14 +14,17 @@ function App() {
 
       <div className="p-5">
         
-         <div className="second-color font-extrabold text-center md:flex flex-col items-center justify-center">
-          <h1 className='text-xl lg:text-3xl'>GOVERNMENT COLLEGE OF ENGINEERING ERODE</h1>
-          <h1 className='text-lg lg:text-xl mt-1'>DEPARTMENT OF COMPUTER SCIENCE AND ENGINEERING</h1>
-          <h1 className='text-lg font-extrabold mt-1'>PROUDLY PRESENTS </h1>
-          {/* <h1 className='text-5xl font-bold mt-5 font-serif'>SYNTAX</h1> */}
+         <div className="second-color  text-center md:flex flex-col items-center justify-center">
+          <h1 className='text-xl font-extrabold lg:text-3xl'>GOVERNMENT COLLEGE OF ENGINEERING ERODE</h1>
+          <h1 className='text-lg font-bold lg:text-xl mt-1'>DEPARTMENT OF COMPUTER SCIENCE AND ENGINEERING</h1>
+          <h1 className='text-lg mt-1'>PROUDLY PRESENTS </h1>
+          
+          <div className="mt-3 flex flex-col items-center justify-center">
+            <img src={syntax} className='h-full w-[70vw] md:w-[50vw]  object-cover' alt="" />
+          </div>
         </div> 
         
-        <div className="mt-60">
+        <div className="">
           <Timer/>
         </div>
 
