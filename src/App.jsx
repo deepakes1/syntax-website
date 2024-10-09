ca85522c4f60e36ef03d348d547c1b0f7f1f729c
Deepak ES <deepakes2003@gmail.com>
2024-10-09 09:25:47 +0530

    15

--- src/App.jsx
+++ src/App.jsx
@@ -12,16 +12,15 @@ function App() {
 
 
       <div className="p-5">
-        {/* clg name */}
-       
+        
          <div className="second-color font-extrabold text-center md:flex flex-col items-center justify-center">
-          <h1 className='text-xl'>GOVERNMENT COLLEGE OF ENGINEERING ERODE</h1>
-          <h1 className='text-lg mt-1'>DEPARTMENT OF COMPUTER SCIENCE AND ENGINEERING</h1>
-          <h1 className='text-lg font-extrabold mt-1'>PRODULY PRESENTS </h1>
-          <h1 className='text-5xl font-bold mt-5 font-serif'>SYNTAX</h1>
+          <h1 className='text-xl lg:text-3xl'>GOVERNMENT COLLEGE OF ENGINEERING ERODE</h1>
+          <h1 className='text-lg lg:text-xl mt-1'>DEPARTMENT OF COMPUTER SCIENCE AND ENGINEERING</h1>
+          <h1 className='text-lg font-extrabold mt-1'>PROUDLY PRESENTS </h1>
+          {/* <h1 className='text-5xl font-bold mt-5 font-serif'>SYNTAX</h1> */}
         </div> 
         
-        <div className="mt-10">
+        <div className="mt-60">
           <Timer/>
         </div>
 
