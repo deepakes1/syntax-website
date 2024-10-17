d9b382f2beab782c87734f5027b48b655cef27a0
Deepak ES <deepakes2003@gmail.com>
2024-10-17 20:42:28 +0530

    leka99

--- src/App.jsx
+++ src/App.jsx
@@ -13,36 +13,35 @@ function App() {
       
   return (
     <>
+      <div className="h-[10vh] bg-red-500">
+        <Navbar />
+      </div>
+      
 
-      <Navbar />
 
-
-      <div className="p-5">
-        
-         <div className="p-5">
-        {/* Make this div occupy full height on mobile */}
-        <div className="h-screen flex flex-col justify-center">
-          <div className="second-color text-center flex flex-col items-center justify-center flex-grow">
-            <h1 className='text-3xl font-extrabold lg:text-3xl'>GOVERNMENT COLLEGE OF ENGINEERING ERODE</h1>
-            <h1 className='text-2xl font-bold lg:text-xl mt-1'>DEPARTMENT OF COMPUTER SCIENCE AND ENGINEERING</h1>
-            <h1 className='text-xl mt-1'>PROUDLY PRESENTS</h1>
-            
-            <div className="mt-5 flex flex-col items-center justify-center">
-              <img src={zinnia} className='h-full w-[80vw] md:w-[50vw] scale-110 md:scale-100 object-cover' alt="" />
-            </div>
-          </div> 
-
-          <div className="mt-10">
-            <Timer />
+      <div className="p-5 h-[80vh] md:h-[70vh] ">
+         <div className="">
+          <div className="second-color  text-center md:flex flex-col items-center justify-center">
+          <h1 className='text-3xl font-extrabold lg:text-3xl'>GOVERNMENT COLLEGE OF ENGINEERING ERODE</h1>
+          <h1 className='text-2xl font-bold lg:text-xl mt-1'>DEPARTMENT OF COMPUTER SCIENCE AND ENGINEERING</h1>
+          <h1 className='text-xl mt-1'>PROUDLY PRESENTS </h1>
+          
+          <div className="mt-5 flex flex-col items-center justify-center">
+            <img src={zinnia} className='h-full w-[80vw]  md:w-[50vw] scale-110 md:scale-100  object-cover' alt="" />
           </div>
+        </div> 
+        
+        <div className="mt-10">
+          <Timer/>
         </div>
+         </div>
 
-        <div className='flex flex-col items-center justify-center mt-32 h-[40vh]'>
+        
+      </div>
+
+      <div className='flex flex-col items-center justify-center mt-32 h-[40vh]'>
           <CardCarousel/>
         </div>
-        </div>
-      
-      </div>
 
     </>
   )
