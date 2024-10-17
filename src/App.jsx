06efa9adb18acbd716464403d786eda7ec754267
Deepak ES <deepakes2003@gmail.com>
2024-10-17 20:21:14 +0530

    lekaa2

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,8 @@ function App() {
 
       <div className="p-5">
         
-         <div className="second-color  text-center md:flex flex-col items-center justify-center">
+         <div className="h-[100vh]">
+          <div className="second-color  text-center md:flex flex-col items-center justify-center">
           <h1 className='text-2xl font-extrabold lg:text-3xl'>GOVERNMENT COLLEGE OF ENGINEERING ERODE</h1>
           <h1 className='text-xl font-bold lg:text-xl mt-1'>DEPARTMENT OF COMPUTER SCIENCE AND ENGINEERING</h1>
           <h1 className='text-lg mt-1'>PROUDLY PRESENTS </h1>
@@ -32,6 +33,7 @@ function App() {
         <div className="mt-10">
           <Timer/>
         </div>
+         </div>
 
         <div className='flex flex-col items-center justify-center mt-32 h-[40vh]'>
           <CardCarousel/>
