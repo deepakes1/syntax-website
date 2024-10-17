51320c59f2ec8be1d3eb2dad28677ff20771c17d
Deepak ES <deepakes2003@gmail.com>
2024-10-17 20:28:46 +0530

    lekaa6

--- src/App.jsx
+++ src/App.jsx
@@ -21,12 +21,12 @@ function App() {
         
          <div className="h-[50vh] md:h-[70vh]">
           <div className="second-color  text-center md:flex flex-col items-center justify-center">
-          <h1 className='text-2xl font-extrabold lg:text-3xl'>GOVERNMENT COLLEGE OF ENGINEERING ERODE</h1>
-          <h1 className='text-xl font-bold lg:text-xl mt-1'>DEPARTMENT OF COMPUTER SCIENCE AND ENGINEERING</h1>
-          <h1 className='text-lg mt-1'>PROUDLY PRESENTS </h1>
+          <h1 className='text-3xl font-extrabold lg:text-3xl'>GOVERNMENT COLLEGE OF ENGINEERING ERODE</h1>
+          <h1 className='text-2xl font-bold lg:text-xl mt-1'>DEPARTMENT OF COMPUTER SCIENCE AND ENGINEERING</h1>
+          <h1 className='text-xl mt-1'>PROUDLY PRESENTS </h1>
           
           <div className="mt-5 flex flex-col items-center justify-center">
-            <img src={zinnia} className='h-full w-[80vw]  md:w-[50vw]  object-cover' alt="" />
+            <img src={zinnia} className='h-full w-[80vw]  md:w-[50vw] scale-110 md:scale-100  object-cover' alt="" />
           </div>
         </div> 
         
