2ed2f3ab741ea737e0bc3c9bb433b1dac5ca8030
Deepak ES <deepakes2003@gmail.com>
2024-10-17 21:30:52 +0530

    leu569

--- src/App.jsx
+++ src/App.jsx
@@ -39,13 +39,27 @@ function App() {
         
       </div>
 
+      
+
+      <div className="p-5 mt-16 bg-[#40377D] text-[#E6E6FA]">
+        <h1 className='text-3xl font-bold text-center  '>About Symposium</h1>
+        <p className='text-lg mt-5 '>Join us for <b>Zinnia</b>, an exciting symposium designed to inspire innovation, collaboration, and growth in computer science and engineering. This unique event unites students, faculty, and industry professionals, offering a platform for aspiring engineers to present their ideas and engage in knowledge-sharing. Zinnia aims to promotes a collaborative environment where participants from various academic levels can explore emerging technologies and discover new possibilities</p>
+      </div>
+
       <div className='flex flex-col items-center justify-center mt-32 h-[40vh]'>
           <CardCarousel/>
         </div>
 
-      <div className="p-5 mt-16">
-        <h1 className='text-3xl font-bold'>About Symposium</h1>
-        <p className='text-lg mt-2'>Join us for Zinnia, an exciting symposium designed to inspire innovation, collaboration, and growth in computer science and engineering. This unique event unites students, faculty, and industry professionals, offering a platform for aspiring engineers to present their ideas and engage in knowledge-sharing. Zinnia aims to promotes a collaborative environment where participants from various academic levels can explore emerging technologies and discover new possibilities</p>
+      {/* event Details */}
+      <div className="mt-16 p-5">
+        <h1 className='text-2xl bg-slate-600 text-center text-white p-3'>Register For Events</h1>
+      </div>
+
+
+      {/* about clg */}
+      <div className="p-5 mt-10 bg-[#40377D] text-[#E6E6FA]">
+        <h1 className='text-3xl font-bold text-center  '>About Our College</h1>
+        <p className='text-lg mt-5 '>Government College of Engineering, Erode (GCE Erode), established in 1984, is a leading engineering institution in Tamil Nadu, India. <b>Affiliated with Anna University</b> , it offers a conducive learning environment on a <b>200-acre campus</b>. Known for its excellent infrastructure and qualified faculty, GCE Erode emphasizes research and boasts a strong record of student placements. It also serves as the <b>Zonal Headquarters</b> for <b>Zone-XI of Anna University</b>, overseeing engineering colleges in the region.</p>
       </div>
 
     </>
