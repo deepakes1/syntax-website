73c2a370905571bf999d3a7b9ca6bbc9293a1b3d
Deepak ES <deepakes2003@gmail.com>
2024-10-17 20:37:11 +0530

    lekaa9

--- src/App.jsx
+++ src/App.jsx
@@ -19,25 +19,29 @@ function App() {
 
       <div className="p-5">
         
-         <div className="h-[60vh] md:h-[70vh] flex flex-col justify-between">
-          <div className="second-color  text-center md:flex flex-col items-center justify-center">
-          <h1 className='text-3xl font-extrabold lg:text-3xl'>GOVERNMENT COLLEGE OF ENGINEERING ERODE</h1>
-          <h1 className='text-2xl font-bold lg:text-xl mt-1'>DEPARTMENT OF COMPUTER SCIENCE AND ENGINEERING</h1>
-          <h1 className='text-xl mt-1'>PROUDLY PRESENTS </h1>
-          
-          <div className="mt-5 flex flex-col items-center justify-center">
-            <img src={zinnia} className='h-full w-[80vw]  md:w-[50vw] scale-110 md:scale-100  object-cover' alt="" />
+         <div className="p-5">
+        {/* Make this div occupy full height on mobile */}
+        <div className="min-h-screen flex flex-col justify-between">
+          <div className="second-color text-center flex flex-col items-center justify-center flex-grow">
+            <h1 className='text-3xl font-extrabold lg:text-3xl'>GOVERNMENT COLLEGE OF ENGINEERING ERODE</h1>
+            <h1 className='text-2xl font-bold lg:text-xl mt-1'>DEPARTMENT OF COMPUTER SCIENCE AND ENGINEERING</h1>
+            <h1 className='text-xl mt-1'>PROUDLY PRESENTS</h1>
+            
+            <div className="mt-5 flex flex-col items-center justify-center">
+              <img src={zinnia} className='h-full w-[80vw] md:w-[50vw] scale-110 md:scale-100 object-cover' alt="" />
+            </div>
+          </div> 
+
+          <div className="mt-10">
+            <Timer />
           </div>
-        </div> 
-        
-        <div className="mt-10">
-          <Timer/>
         </div>
-         </div>
 
         <div className='flex flex-col items-center justify-center mt-32 h-[40vh]'>
           <CardCarousel/>
         </div>
+        </div>
+      
       </div>
 
     </>
