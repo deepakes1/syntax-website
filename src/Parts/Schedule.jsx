5a5e401c77777d30837ed9b4322bdf40b369178c
Deepak ES <deepakes2003@gmail.com>
2024-10-25 23:52:17 +0530

    lunch

--- src/Parts/Schedule.jsx
+++ src/Parts/Schedule.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 
 function Schedule() {
   return (
+    <>
     <div className='mt-32'>
+        <h1 className='text-4xl font-bold text-center'>Day 1</h1>
       <table border="1">
         <thead>
           <tr>
@@ -67,6 +69,82 @@ function Schedule() {
         </tbody>
       </table>
     </div>
+
+    <div className='mt-10'>
+        <h1 className='text-4xl font-bold text-center'>Day 2</h1>
+      <table border="1">
+        <thead>
+          <tr>
+            <th style={{ border: "2px solid black", padding: "10px" }}>S.NO</th>
+            <th style={{ border: "2px solid black", padding: "10px" }}>EVENTS</th>
+            <th style={{ border: "2px solid black", padding: "10px" }}>START TIME</th>
+            <th style={{ border: "2px solid black", padding: "10px" }}>END TIME</th>
+            <th style={{ border: "2px solid black", padding: "10px" }}>VENUE</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td style={{ border: "2px solid black", padding: "10px" }}>1</td>
+            <td style={{ border: "2px solid black", padding: "10px" }}>HACKATHON</td>
+            <td style={{ border: "2px solid black", padding: "10px" }}>9:30 AM</td>
+            <td style={{ border: "2px solid black", padding: "10px" }}>3:30 PM</td>
+            <td style={{ border: "2px solid black", padding: "10px" }}>CLASS-104, SEMINAR HALL</td>
+          </tr>
+          <tr>
+            <td style={{ border: "2px solid black", padding: "10px" }}>2</td>
+            <td style={{ border: "2px solid black", padding: "10px" }}>CODE WARS ROUND 1</td>
+            <td style={{ border: "2px solid black", padding: "10px" }}>9:30 AM</td>
+            <td style={{ border: "2px solid black", padding: "10px" }}>12:30 PM</td>
+            <td style={{ border: "2px solid black", padding: "10px" }}>LAB CC1, CC2</td>
+          </tr>
+          <tr>
+          <td style={{ border: "2px solid black", padding: "10px" }}>3</td>
+            <td style={{ border: "2px solid black", padding: "10px" }}>CODE WARS ROUND 2</td>
+            <td style={{ border: "2px solid black", padding: "10px" }}>2:00 PM</td>
+            <td style={{ border: "2px solid black", padding: "10px" }}>3:00 PM</td>
+            <td style={{ border: "2px solid black", padding: "10px" }}>LAB CC1, CC2</td>
+          </tr>
+          <tr>
+          <td style={{ border: "2px solid black", padding: "10px" }}>4</td>
+            <td style={{ border: "2px solid black", padding: "10px" }}>TECHXCELERATE ROUND 1</td>
+            <td style={{ border: "2px solid black", padding: "10px" }}>9:30 AM</td>
+            <td style={{ border: "2px solid black", padding: "10px" }}>10:30 PM</td>
+            <td style={{ border: "2px solid black", padding: "10px" }}>CLASS-101</td>
+          </tr>
+          
+          <tr>
+          <td style={{ border: "2px solid black", padding: "10px" }}>5</td>
+            <td style={{ border: "2px solid black", padding: "10px" }}>TECHXCELERATE ROUND 1</td>
+            <td style={{ border: "2px solid black", padding: "10px" }}>10:30 AM</td>
+            <td style={{ border: "2px solid black", padding: "10px" }}>12:00 PM</td>
+            <td style={{ border: "2px solid black", padding: "10px" }}>CLASS-101</td>
+          </tr>
+          <tr>
+          <td style={{ border: "2px solid black", padding: "10px" }}>6</td>
+            <td style={{ border: "2px solid black", padding: "10px" }}>VISUAL DESIGN</td>
+            <td style={{ border: "2px solid black", padding: "10px" }}>11:00 AM</td>
+            <td style={{ border: "2px solid black", padding: "10px" }}>12:30 PM</td>
+            <td style={{ border: "2px solid black", padding: "10px" }}>CLASS-103</td>
+          </tr>
+          <tr>
+          <td style={{ border: "2px solid black", padding: "10px" }}>7</td>
+            <td style={{ border: "2px solid black", padding: "10px" }}>MEME CREATION</td>
+            <td style={{ border: "2px solid black", padding: "10px" }}>2:00 PM</td>
+            <td style={{ border: "2px solid black", padding: "10px" }}>3:00 PM</td>
+            <td style={{ border: "2px solid black", padding: "10px" }}>CLASS-102</td>
+          </tr>
+          <tr>
+            <td style={{ border: "2px solid black", padding: "10px" }}>8</td>
+            <td style={{ border: "2px solid black", padding: "10px" }}>SPHERE CLASH</td>
+            <td style={{ border: "2px solid black", padding: "10px" }}>2:30 PM</td>
+            <td style={{ border: "2px solid black", padding: "10px" }}>3:30 PM</td>
+            <td style={{ border: "2px solid black", padding: "10px" }}>CLASS-103</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    </>
+    
   );
 }
 
